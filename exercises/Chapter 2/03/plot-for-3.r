8e734b54135aeb9d5639a6967cf3e361b8de1fa0
aidanlokeeffe <42976464+aidanlokeeffe@users.noreply.github.com>
2023-03-06 14:48:15 -0600

    Pushing work from this morning

--- exercises/Chapter 2/03/plot-for-3.r
+++ exercises/Chapter 2/03/plot-for-3.r
@@ -1,13 +1,38 @@
 library(tidyverse)
 
-df = 1:10
-bias2 = 2/(df^2)
-variance = df^2 + .5
-train = 2.75/df + abs(rnorm(10, mean=0, sd=.333))
-test = bias2 + variance + 0.25 + abs(rnorm(10, mean=0, sd=.333))
-bayes = rep(0.25, 10)
+# Generate the curves to be plotted
+df = 1:100
+bias2 = 10/(df^0.4)
+variance = 0.006*((df-1)/2)^2 + 3.1
+train = 11/(df^0.36)
+bayes = rep(1.84, 100)
+test = bias2 + variance + bayes
 
-data = data.frame(df, bias2, variance, train, test)
-View(data)
+data = data.frame( rep(1:100, 5), rep(c("bias^2", "variance", "training_error", "testing_error", "bayes_error"), each=100)  )
+data = data.frame(data, c(bias2,variance,train,test,bayes  ))
+names(data) = c("df", "Variable", "Value")
+
+# Plot them
+plt <- ggplot(data=data, aes(x=df, y=Value, group=Variable))+
+  geom_line(aes(color=Variable), linewidth=1)+
+  labs(x="df", y="Value")+
+  ggtitle("Various Error Values vs. Degrees of Freedom")
+
+plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
